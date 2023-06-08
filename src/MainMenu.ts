b412d025989aff3e9160c954e409c24d5ce6f085
Christian Fleury <chfleurysiq@gmail.com>
2023-06-08 14:25:19 -0300

    feat: add MainMenu
    Co-authored-by: Cícero Fernandes <ciceroff@users.noreply.github.com>

---
+++ src/MainMenu.ts
@@ -0,0 +1,30 @@
+import inquirer from 'inquirer';
+
+enum AvailableChoices {
+  TEMPLATE = 'template',
+  OBSERVER = 'observer',
+}
+
+export default class MainMenu {
+  constructor() {
+    this.run();
+  }
+
+  async run() {
+    const pick: Record<'mainMenu', AvailableChoices> = await inquirer.prompt({
+      name: 'mainMenu',
+      type: 'list',
+      message: 'Selecione uma opção.\n',
+      choices: Object.values(AvailableChoices),
+    });
+
+    switch (pick.mainMenu) {
+      case AvailableChoices.OBSERVER:
+        break;
+      case AvailableChoices.TEMPLATE:
+        break;
+      default:
+        break;
+    }
+  }
+}

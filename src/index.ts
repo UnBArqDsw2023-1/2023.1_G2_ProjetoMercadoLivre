d2a520c333c3f6ef356bc1b73d4298b92f12ea76
Christian Fleury <chfleurysiq@gmail.com>
2023-06-11 11:33:24 -0300

    feat: add container logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-import MainMenu from './MainMenu.js';
+import { Container } from 'inversify';
+import MainMenu, { AvailableChoices } from './MainMenu.js';
+import { ClientsToken } from './ExamplesFactory.js';
+import TemplateClient from './template/TemplateClient.js';
+import AdapterClient from './adapter/AdapterClient.js';
+import ContentDecoratorClient from './contentDecorator/ContentDecoratorClient.js';
+import { Client } from './interfaces/Client.js';
+export const container = new Container();
+
+container
+  .bind<Client>(ClientsToken)
+  .to(TemplateClient)
+  .whenTargetNamed(AvailableChoices.TEMPLATE);
+
+container
+  .bind<Client>(ClientsToken)
+  .to(AdapterClient)
+  .whenTargetNamed(AvailableChoices.ADAPTER);
+
+container
+  .bind<Client>(ClientsToken)
+  .to(ContentDecoratorClient)
+  .whenTargetNamed(AvailableChoices.DECORATOR);
 
 new MainMenu();

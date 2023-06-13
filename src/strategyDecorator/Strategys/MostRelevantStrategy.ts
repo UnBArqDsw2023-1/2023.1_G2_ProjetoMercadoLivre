7020984f97a133643e2d8d5cb033535344c6ab92
Gabriel <gabriel.souza@terceiro.rnp.br>
2023-06-12 22:44:49 -0300

    fix: nome de classe incorreto

--- src/strategyDecorator/Strategys/MostRelevantStrategy.ts
+++ src/strategyDecorator/Strategys/MostRelevantStrategy.ts
@@ -1,7 +1,7 @@
 import Avaliation from '../Avaliation.js';
 import FilterStrategy from '../interface/FilterStrategy.js';
 
-export default class MostUsefulStrategy implements FilterStrategy {
+export default class MostRelevantStrategy implements FilterStrategy {
   filter(avaliations: Avaliation[]): Avaliation[] {
     return avaliations.sort((a, b) => b.votesQtd - a.votesQtd);
   }

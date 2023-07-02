1af7a770f544b1c0ef4039e45c62498636aa3e51
Letícia <72623771+LeticiaAires@users.noreply.github.com>
2023-07-02 12:11:31 -0300

    Update clothesAvaliation.py

--- src/observerAbstract/abstractFactory/clothesAvaliation.py
+++ src/observerAbstract/abstractFactory/clothesAvaliation.py
@@ -1,1 +1,13 @@
+from avaliation import Avaliation
+
+class ClothesAvaliation(Avaliation):
+    def __init__(self, buyerId: int, productId: int, score: float, questions: list[str]):
+        self.buyerId = buyerId
+        self.productId = productId
+        self.score = score
+        self.questions = questions
+
+    def avaliate(self, b: Buyer, p: Product) -> bool:
+        # preciso pensar na lógica específica dos critérios de avaliação para as roupas
+    
 

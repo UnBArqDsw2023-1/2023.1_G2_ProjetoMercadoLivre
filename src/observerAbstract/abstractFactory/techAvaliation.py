4455f499b4c0c5b539dd53aafe1138a76e48e7aa
Letícia <72623771+LeticiaAires@users.noreply.github.com>
2023-07-02 12:15:13 -0300

    Update techAvaliation.py

--- src/observerAbstract/abstractFactory/techAvaliation.py
+++ src/observerAbstract/abstractFactory/techAvaliation.py
@@ -1,1 +1,11 @@
+from avaliation import Avaliation
 
+class TechAvaliation(Avaliation):
+    def __init__(self, buyerId: int, productId: int, score: float, questions: list[str]):
+        self.buyerId = buyerId
+        self.productId = productId
+        self.score = score
+        self.questions = questions
+
+    def avaliate(self, b: Buyer, p: Product) -> bool:
+        # Preciso pensar na lógica específica dos critérios de avaliação para tecnologia
